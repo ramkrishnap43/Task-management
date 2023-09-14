6a6b4c0c59de2d48f67c9266bbc195dcb5ffc3c5
ramkrishnap43 <ramkrishnap.prabhu43@gmail.com>
2023-09-14 11:49:22 +0530

    final

--- task-management/src/Components/Project.jsx
+++ task-management/src/Components/Project.jsx
@@ -1,60 +1,63 @@
-import React, { useState } from 'react';
-import "./Project.css"
+import React, { useState } from "react";
+import "./Project.css";
 
 const Project = () => {
-    const [task,setTask] = useState([]);
-    const [data, setData] = useState('');
-    const [editTask, setEditTask] = useState(null);
+  const [task, setTask] = useState([]);
+  const [data, setData] = useState("");
+  const [editTask, setEditTask] = useState(null);
 
-    const addTask = () => {
-        if(data.trim() !== ""){
-            if(editTask === null){
-                setTask([...task,data]);
-
-            }
-            else{
-                const updateTask = [...task];
-                updateTask[editTask] = data;
-                setTask(updateTask);
-                setEditTask(null)
-            }
-            setData('')
-        }
+  const addTask = () => {
+    if (data.trim() !== "") {
+      if (editTask === null) {
+        setTask([...task, data]);
+      } else {
+        const updateTask = [...task];
+        updateTask[editTask] = data;
+        setTask(updateTask);
+        setEditTask(null);
+      }
+      setData("");
     }
+  };
 
-    const editedTask = (i) => {
-        setData(task[i])
-        setEditTask(i)
-
-    }
-    const deletedTask = (index) => {
-        const updatedTask = [...task];
-        updatedTask.splice(index,1)
-        setTask(updatedTask)
-    }
-
+  const editedTask = (i) => {
+    setData(task[i]);
+    setEditTask(i);
+  };
+  const deletedTask = (index) => {
+    const updatedTask = [...task];
+    updatedTask.splice(index, 1);
+    setTask(updatedTask);
+  };
 
   return (
-    <div className='container'>
-        <h1>Task Management System</h1>
-        <div>
-            <input 
-                type='text'
-                placeholder='Enter task here'
-                value={data}
-                onChange={(e) => setData(e.target.value)}
-            />
-            <button>
-                {editTask === null ? "Add Task" : "Update Task"}
-            </button>
-        </div>
-        <div>
-            <ul></ul>
-        </div>
-
-
+    <div className="container">
+      <h1>Task Management System</h1>
+      <div>
+        <input
+          className="inputBox"
+          type="text"
+          placeholder="Enter task here"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
+        <button className="button" onClick={addTask}>
+          {editTask === null ? "Add Task" : "Update Task"}
+        </button>
+      </div>
+      <div>
+        <ul>
+          {task.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => editedTask(index)}>Edit</button>
+              <button onClick={() => deletedTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
